0664a9feda7ab9c7b1716ac2a0d1d730fce9cb71
technical6269 <saurabhdhakad6269@gmail.com>
2026-04-18 22:55:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,61 +1,16 @@
-
-import { useState } from 'react';
+import React, { Component } from 'react'
 import './App.css';
-import Navbar from './component/Navbar';
-import TextForm from './component/TextForm';
-import About from './component/About';
-import Alert from './component/Alert';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-function App() {
-  const [mode, setMode] = useState('light')
- const[alert, setAlert] = useState(null)
-  const showAlert = (message, type)=>{
-  setAlert({
-      msg: message,
-      type: type
-  })
-  setTimeout(()=>{
-    setAlert(null);
-  }, 1500)
- }
-  const toggleMode= ()=>{
-    if(mode === 'light'){
-      setMode('dark')
-      document.body.style.backgroundColor= '#042743';
-      //  document.title="textutils dark mode";
-      showAlert("Dark mode has been enabled", "success");
-      // setInterval(() => {
-      //     document.title="Textutils is amazing";
-      //   }, 2000);
-      //    setInterval(() => {
-      //     document.title="Install textutils now ";
-      //   }, 1500);
-    }
-    else {
-      setMode('light')
-       document.body.style.backgroundColor= 'white';
-        // document.title="textutils light mode";
-        showAlert("Light mode has been enabled", "success");
-    }
+import NavBar from './component/NavBar';
+import News from './component/News';
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar/>
+        <News/>
+      </div>
+    )
   }
-  return (
-     <>
-     <Router>
- <Navbar tittle= "Textutils" aboutText= "About" mode={mode} toggleMode={toggleMode}/> 
- <Alert alert={alert}/>
-  <div className="container my-3">
-<Routes>
-<Route exact path="/about"
-    element={<About mode={mode}/>} />
-<Route path="/"  
-    element={<TextForm showAlert={showAlert} heading="Try Textutils - Word counter, Character counter, Remove extra spaces" mode={mode}/> } />
-  
- </Routes> 
-  </div>
-   </Router>
-   </>
-  );
 }
 
-export default App;
+export default App
